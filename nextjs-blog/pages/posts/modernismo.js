583cb4e74f28bc2e135e5735fe8b477b86c510e5
Scab647 <80284437+Scab647@users.noreply.github.com>
2021-03-15 00:15:24 -0600

    Todos los posts

--- nextjs-blog/pages/posts/modernismo.js
+++ nextjs-blog/pages/posts/modernismo.js
@@ -2,18 +2,95 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
 
-export default function FirstPost() {
+export default function Clasicismo() {
   return (
-    <Layout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </Layout>
+    <div className="container">
+        <Head>
+        <title>Modernismo</title>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+
+        <div className="description">
+
+        <a href="/" className="description">
+          <img src="https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/home-512.png">
+          </img> 
+        </a>     
+          
+        </div> 
+
+        <h1 className="title">
+          Modernismo
+        </h1>
+
+        <p className="description">
+        Se refiere a la música de la tradición europea escrita, elaborada 
+aproximadamente entre 1910 y 1975. La música  moderna está basada 
+en los  valores  filosóficos  y estéticos del  modernismo el cual 
+tiene como  principio  principal  la ruptura con  la tradición, y 
+la permanente innovación.
+
+       </p>
+        
+       <p className="description">
+       El modernismo musical tiene  tres  características principales que 
+lo distinguen de los períodos anteriores:
+       </p>
+
+       <p className="description">
+       -La expansión o abandono de la tonalidad.
+       </p>
+
+       <p className="description">
+-El uso de las técnicas extendidas.
+
+       </p>
+
+       <p className="description">
+-La incorporación de sonidos y ruidos novedosos en la composición.
+       </p>
+
+    
+    
+       <h4 className="description">
+       ¿A qué compositor del modernismo quieres escuchar?
+        </h4>
+
+      <div className="grid">
+         <a href="/posts/clasicismo" className="card3">
+         <h5>STRAVINSKY</h5>
+         </a>
+
+         <a href="/posts/romanticismo" className="card3">
+         <h5>MESSIAEN</h5>
+         </a>
+        </div>
+
+       <style jsx>{`
+    p{
+      text-align:justify;
+      margin-left: 10rem;
+      margin-right: 10rem;
+      color: #0f0a03;
+    }
+
+    img{
+
+    }
+`}</style>
+        </main>
+        <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+    </div>
   )
 }
--- nextjs-blog/pages/posts/romanticismo.js
+++ nextjs-blog/pages/posts/romanticismo.js
@@ -2,18 +2,97 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
 
-export default function FirstPost() {
+export default function Clasicismo() {
   return (
-    <Layout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </Layout>
+    <div className="container">
+        <Head>
+        <title>Romanticismo</title>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+
+        <div className="description">
+
+        <a href="/" className="description">
+          <img src="https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/home-512.png">
+          </img> 
+        </a>     
+          
+        </div> 
+
+        <h1 className="title">
+          Romanticismo/Postromanticismo
+        </h1>
+
+        <p className="description">
+        El romanticismo musical se caracterizó por la reevaluación de la
+música instrumental. Ésta pasó a ser pensada como arte  autónomo 
+capaz de expresar lo inexpresable.  El compositor  comenzó a ser 
+visto como el genio intermediario entre  la audiencia y el mundo
+espiritual  al  que daba acceso cada obra musical.  Por ende, el 
+carácter  sentimental y  expresivo  de  la  música se hizo mucho 
+más evidente.
+
+       </p>
+        
+       <p className="description">
+       Su consecuencia directa, el Postromanticismo, no fue más que una 
+prolongación  del   Romanticismo,  donde  las  reglas  musicales 
+comenzaron  a  ser  moldeadas  a   placer  de  los  compositores, 
+impulsados  por   las  ideas   filosóficas   de Nietszche  y  el 
+nacionalismo alemán. 
+
+
+       </p>
+    
+       <h4 className="description">
+       ¿Qué te atrajo más?
+        </h4>
+
+        <div className="grid">
+         <p className="card4">
+         <h4>Romanticismo</h4>
+         </p>
+
+         <p className="card4">
+         <h4>Postromanticismo</h4>
+         </p>
+        </div>  
+
+      <div className="grid">
+         <a href="/posts/clasicismo" className="card3">
+         <h5>TCHAIKOVSKY</h5>
+         </a>
+
+         <a href="/posts/romanticismo" className="card3">
+         <h5>MAHLER</h5>
+         </a>
+        </div>
+
+       <style jsx>{`
+    p{
+      text-align:justify;
+      margin-left: 10rem;
+      margin-right: 10rem;
+      color: #0f0a03;
+    }
+
+    img{
+
+    }
+`}</style>
+        </main>
+        <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+    </div>
   )
 }
