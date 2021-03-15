50efe36f4ce6c4f09cebe11b5b19a1ce06694a31
Scab647 <80284437+Scab647@users.noreply.github.com>
2021-03-14 22:37:16 -0600

    Inicio de página-clasicismo

--- nextjs-blog/pages/posts/clasicismo.js
+++ nextjs-blog/pages/posts/clasicismo.js
@@ -2,18 +2,27 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
 
-export default function FirstPost() {
+export default function Clasicismo() {
   return (
-    <Layout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </Layout>
+    <div className="container">
+        <Head>
+        <title>Clasicismo</title>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+        <h1 className="title">
+          Clasicismo
+        </h1>
+
+        <p className="description">
+        Desarrollado aproximadamente entre 1750 y 1820   por compositores 
+como Joseph Haydn, Wolfgang Amadeus Mozart y Ludwig van Beethoven, 
+el clasicismo se  caracterizó  por  el  gusto por lo  natural, lo 
+equilibrado y lo claro; rechazando del artificio y  el  exceso de 
+sofisticación de la música barroca. </p>
+        
+        </main>
+    </div>
   )
 }
